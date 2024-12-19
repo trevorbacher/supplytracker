e6c130148f08dc0970177ef78817dc4ccb0d7be0
teisenba <teisenba@vols.utk.edu>
2024-12-19 11:27:06 -0500

    cleaned up formatting

--- supplytracker-app/backend/server.js
+++ supplytracker-app/backend/server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:3000',
   credentials: true
 }));
 
--- supplytracker-app/frontend/src/app/layout.js
+++ supplytracker-app/frontend/src/app/layout.js
@@ -3,23 +3,23 @@ import './globals.css';
 import './layout.css';
 
 const Layout = ({ children }) => {
-    return (
-        <html lang="en">
-            <body>
-                <div className="layout">
-                    <header>
-                        <nav>
-                            {/* Navigation links can go here */}
-                        </nav>
-                    </header>
-                    <main>{children}</main>
-                    <footer>
-                        <p>&copy; {new Date().getFullYear()} UTK BCMB Supply Tracker. All rights reserved.</p>
-                    </footer>
-                </div>
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en">
+			<body>
+				<div className="layout">
+					<header>
+						<nav>
+							{/* Navigation links can go here */}
+						</nav>
+					</header>
+					<main>{children}</main>
+					<footer>
+						<p>&copy; {new Date().getFullYear()} UTK BCMB Supply Tracker. All rights reserved.</p>
+					</footer>
+				</div>
+			</body>
+		</html>
+	);
 };
 
 export default Layout;
