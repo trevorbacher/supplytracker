b84f2612be951fae607615b49f0dfd673dae0c44
teisenba <teisenba@vols.utk.edu>
2024-12-18 19:59:33 -0500

    proof-of-concept frontend added

--- supplytracker-app/backend/server.js
+++ supplytracker-app/backend/server.js
@@ -14,7 +14,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 // Routes Middlewares
 app.use("/api/users", userRoute);
